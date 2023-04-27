091b72e41c514450d7bb2fee7ad5969a0620525f
Kirieshka777 <kmelnikov2011@gmail.com>
2023-04-27 19:56:20 +0300

    поправки(не в конституции)

--- numplay.py
+++ numplay.py
@@ -1,4 +1,17 @@
-
-from datetime import datetime
-a = datetime.now()
-print(a.hour, ':', a.minute)
+import datetime as dt
+UTC = {
+    'Ростов на дону': 3,
+    'Москва':3,
+    'Саратов':4,
+    'Петропавловск Камчатский':12
+}
+def kgj():
+    city = input('')
+    utc_time = dt.datetime.utcnow()
+    try:
+        time = dt.timedelta(hours=UTC[city])
+        city_time = utc_time + time
+        print(city_time)
+    except KeyError:
+        print('Я не знаю')
+kgj()
